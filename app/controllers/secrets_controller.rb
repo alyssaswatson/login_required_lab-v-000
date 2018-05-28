c4ca799ae1ece6944c63d5f5d7580a71a07f4dff
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-05-28 21:29:49 +0000

    automatically backed up by learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -2,6 +2,10 @@ class SecretsController < ApplicationController
   before_action :require_login
   skip_before_action :require_login, only: [:index]
 
+  def show
+
+  end
+
 private
   def require_login
     return head(:forbidden) unless session.include? :name
